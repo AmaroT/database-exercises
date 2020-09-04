398d127fd83ecd4d4df58595e5d099c43751552c
Amaro Terrazas <42896111+AmaroT@users.noreply.github.com>
2020-09-04 12:17:06 -0500

    created test database

---
+++ albums_migration.sql
@@ -0,0 +1,10 @@
+USE codeup_test_db;
+CREATE TABLE IF NOT EXISTS 'albums' (
+    'id' int(10) unsigned NOT NULL AUTO_INCREMENT,
+    'artist' varchar(50) DEFAULT NULL,
+    'name' varchar(100) DEFAULT NULL,
+    'release_date' date DEFAULT NULL,
+    'sales' int DEFAULT NULL,
+    'genre' varchar(50) DEFAULT NULL,
+    PRIMARY KEY ('id')
+    );
