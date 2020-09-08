51e88acb3a6040dc2a03beeb11c1c526fcc68914
Amaro Terrazas <42896111+AmaroT@users.noreply.github.com>
2020-09-08 18:53:05 -0500

    push daily exercise

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -69,5 +69,3 @@ SELECT * FROM employees WHERE last_name LIKE 'E%' ORDER BY emp_no DESC;
 SELECT *
 FROM employees
 WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31' AND (birth_date LIKE '%12-25%') ORDER BY birth_date, hire_date DESC;
-
--- Part 2
