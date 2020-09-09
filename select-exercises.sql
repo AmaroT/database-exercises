dd78721b763f45a2dc1146f3989ee44e5ce2750e
Amaro Terrazas <42896111+AmaroT@users.noreply.github.com>
2020-09-08 20:45:18 -0500

    completed Basic Statement Exercises refactor

--- select-exercises.sql
+++ select-exercises.sql
@@ -6,20 +6,20 @@ FROM albums
 WHERE artist = 'Pink Floyd';
 
 # The year Sgt. Peppers Lonely Hearts Club Band was released
-SELECT released FROM albums WHERE album = 'Sgt. Peppers Lonely Hearts Club Band';
+SELECT release_date FROM albums WHERE albums.name = 'Sgt. Peppers Lonely Hearts Club Band';
 
 # The genre for Nevermind
-SELECT genre FROM albums WHERE album = 'Nevermind';
+SELECT genre FROM albums WHERE albums.name = 'Nevermind';
 
 # Which albums were released in the 1990s
-SELECT album, released FROM albums WHERE  released BETWEEN 1990 and 1999;
+SELECT albums.name, release_date FROM albums WHERE  release_date BETWEEN 1990 and 1999;
 
 # Which albums had less than 20 million certified sales
-SELECT album, sales FROM albums WHERE sales < 20;
+SELECT albums.name, sales FROM albums WHERE sales < 20;
 
 # All the albums with a genre of "Rock". Why do these query results not include albums with a "Hard Rock" or "Progressive rock"?
-SELECT album, genre FROM albums WHERE genre = 'Rock';
+SELECT albums.name, genre FROM albums WHERE genre = 'Rock';
 # The results will not display anything not equal to "Rock". You have to incoporate a LIKE statement and a wildcard to include anything result
 # with the word "Rock" in the results.LIKE
 
-SELECT  album, genre FROM albums WHERE genre LIKE '%ock%';
+SELECT  albums.name, genre FROM albums WHERE genre LIKE '%ock%';
--- update_exercises.sql
+++ update_exercises.sql
@@ -1,19 +1,19 @@
 USE codeup_test_db;
 
 # All albums in your table.
-SELECT album AS 'Albums in your table' FROM albums;
+SELECT albums.name AS 'Albums in your table' FROM albums;
 
 # All albums released before 1980
-SELECT album AS 'Albums released before 1980' FROM albums WHERE released < 1980;
+SELECT albums.name AS 'Albums released before 1980' FROM albums WHERE release_date < 1980;
 
 # All albums by Michael Jackson
-SELECT album AS 'Released by Michael Jackson' FROM albums WHERE artist = 'Michael Jackson';
+SELECT albums.name  AS 'Released by Michael Jackson' FROM albums WHERE artist = 'Michael Jackson';
 
 # Make all the albums 10 more popular (sales *10)
 UPDATE albums SET sales = (sales * 10);
 
 # Move all the albums before 1980 back to the 1800s.
-UPDATE albums SET released = (released -100) WHERE released < 1980;
+UPDATE albums SET release_date = (release_date -100) WHERE release_date < 1980;
 
 #Change 'Michael Jackson' to 'Peter Jackson'
 UPDATE albums SET artist = 'Peter Jackson' WHERE artist = 'Michael Jackson';
