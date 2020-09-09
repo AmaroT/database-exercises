89bc86a83f6310d5e9ec19be703845b7d5dd3de2
Amaro Terrazas <42896111+AmaroT@users.noreply.github.com>
2020-09-08 20:40:48 -0500

    completed Basic Statement Exercises

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -2,8 +2,8 @@ USE codeup_test_db;
 
 #Albums released after 1991
 
-SELECT album AS 'Deleting albums released after 1991' FROM albums;
-DELETE FROM albums WHERE released > 1991;
+SELECT release_date AS 'Deleting albums released after 1991' FROM albums;
+DELETE FROM albums WHERE release_date > 1991;
 
 #Albums with the genre 'disco'
 SELECT genre AS 'Deleting all records in Disco' FROM albums;
@@ -11,4 +11,4 @@ DELETE FROM albums WHERE genre = 'Disco';
 
 # Albums by 'Whitney Houston' (...or maybe an artist or your choice)
 SELECT artist AS 'Deleting all records by Whitney Houston' FROM albums;
-DELETE FROM albums WHERE artist = 'Whitney Houston';
+DELETE FROM albums WHERE artist LIKE '%Whitney Houston%';
